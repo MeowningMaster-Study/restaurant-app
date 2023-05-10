f4cc3ccb14bd249c350d818a9aac1a86543b2a18
MeowningMaster <meowningmaster@gmail.com>
2023-05-10 21:48:22 +0300

    save

--- backend-jakarta-ee/src/main/kotlin/com/example/backendjakartaee/MainApplication.kt
+++ backend-jakarta-ee/src/main/kotlin/com/example/backendjakartaee/MainApplication.kt
@@ -4,4 +4,4 @@ import jakarta.ws.rs.ApplicationPath
 import jakarta.ws.rs.core.Application
 
 @ApplicationPath("/api")
-class HelloApplication : Application() {}
+class MainApplication : Application()
--- backend-jakarta-ee/src/main/kotlin/com/example/backendjakartaee/SqlService.kt
+++ backend-jakarta-ee/src/main/kotlin/com/example/backendjakartaee/SqlService.kt
@@ -2,29 +2,21 @@ package com.example.backendjakartaee
 
 import java.sql.Connection
 import java.sql.DriverManager
-import java.sql.SQLException
 
 object SqlService {
     private var isInstantiated = false
     private lateinit var connection: Connection
+
     private fun instantiate() {
-        if (!isInstantiated) {
+        if (isInstantiated) {
             return
         }
-        try {
-            Class.forName("org.postgresql.Driver")
-        } catch (e: ClassNotFoundException) {
-            throw RuntimeException(e)
-        }
-        try {
-            connection = DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/postgres",
-                "postgres",
-                "changeme"
-            )
-        } catch (e: SQLException) {
-            throw RuntimeException(e)
-        }
+        Class.forName("org.postgresql.Driver")
+        connection = DriverManager.getConnection(
+            "jdbc:postgresql://localhost:5432/postgres",
+            "postgres",
+            "changeme"
+        )
     }
 
     fun getConnection(): Connection {
