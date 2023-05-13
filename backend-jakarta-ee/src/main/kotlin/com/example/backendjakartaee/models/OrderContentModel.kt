cfc5a195b4f04e3753d23a4b377040668ee05f63
MeowningMaster <meowningmaster@gmail.com>
2023-05-13 15:25:57 +0300

    fix json deserialization

--- backend-jakarta-ee/src/main/kotlin/com/example/backendjakartaee/models/OrderContentModel.kt
+++ backend-jakarta-ee/src/main/kotlin/com/example/backendjakartaee/models/OrderContentModel.kt
@@ -4,12 +4,12 @@ import jakarta.json.bind.annotation.JsonbCreator
 import jakarta.json.bind.annotation.JsonbProperty
 
 data class OrderContentCreateModel @JsonbCreator constructor(
-    @JsonbProperty("menuItem") var menuItemId: Int,
+    @JsonbProperty("menuItemId") var menuItemId: Int,
     @JsonbProperty("quantity") var quantity: Int
 )
 
 data class OrderContentModel @JsonbCreator constructor(
     @JsonbProperty("orderId") var orderId: Int,
-    @JsonbProperty("menuItem") var menuItemId: Int,
+    @JsonbProperty("menuItemId") var menuItemId: Int,
     @JsonbProperty("quantity") var quantity: Int
 )
